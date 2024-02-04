0ef1813f613f32748c8e3ca1afe58b6e8de0956f
Asher Stoppard <asher@asherstoppard.com>
2024-02-04 22:52:39 +0000

    test(experience): add types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,15 @@ export interface ISkill {
   name: string
   skills: string[]
 }
+
+export interface IExperience {
+  isMainConsultancy: boolean
+  from: string
+  to: string
+  companyName: string
+  position: string
+  companyLogo: string
+  tags: string[]
+  location: string
+  skills: string | string[]
+}

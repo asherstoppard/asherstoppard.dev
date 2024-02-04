d35ed6cf3074bee4b1e0288504f2564a8fbd172b
Asher Stoppard <asher@asherstoppard.com>
2024-02-04 21:15:41 +0000

    feat(registry): add export for registry

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -4,7 +4,7 @@ import React, { PropsWithChildren, useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
-export default function StyledComponentsRegistry({ children }: PropsWithChildren) {
+export const StyledComponentsRegistry = ({ children }: PropsWithChildren) => {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
 
   useServerInsertedHTML(() => {
